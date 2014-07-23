2b2a2e8539de17315e8be5b5dcbf5e562c2b5599
Garhan Attebury <garhan.attebury@unl.edu>
2014-07-23 16:58:58 -0500

    seriously, step away from the keyboard at this point

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,7 +2,7 @@ class hccrepo::params {
 
     $hccrepo_baseurl = "http://t2.unl.edu/store/repos/hcc/${::lsbmajdistrelease}/hcc-el${::lsbmajdistrelease}/\$basearch"
     $hccrepo_enabled = true
-    $hccrepo_testing_baseurl = 'http://t2.unl.edu/store/repos/hcc/${::lsbmajdistrelease}/hcc-el${::lsbmajdistrelease}-testing/\$basearch"
+    $hccrepo_testing_baseurl = "http://t2.unl.edu/store/repos/hcc/${::lsbmajdistrelease}/hcc-el${::lsbmajdistrelease}-testing/\$basearch"
     $hccrepo_testing_enabled = false
 
 }
