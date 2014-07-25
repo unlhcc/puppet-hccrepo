5d522ec42b4fea85abbf7cf25b61301731748515
Garhan Attebury <garhan.attebury@unl.edu>
2014-07-25 16:22:14 -0500

    apparently true/false are hard for some combos of ruby/EL/puppet we have

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,8 +1,8 @@
 class hccrepo::params {
 
     $hccrepo_baseurl = "http://t2.unl.edu/store/repos/hcc/${::lsbmajdistrelease}/hcc-el${::lsbmajdistrelease}/\$basearch"
-    $hccrepo_enabled = true
+    $hccrepo_enabled = 1
     $hccrepo_testing_baseurl = "http://t2.unl.edu/store/repos/hcc/${::lsbmajdistrelease}/hcc-el${::lsbmajdistrelease}-testing/\$basearch"
-    $hccrepo_testing_enabled = false
+    $hccrepo_testing_enabled = 0
 
 }
