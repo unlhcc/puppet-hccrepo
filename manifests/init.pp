6742a447f9a53527d65361c2ec2e912a12282a85
Garhan Attebury <garhan.attebury@unl.edu>
2014-07-28 12:20:02 -0500

    added priority support

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,8 +7,10 @@
 class hccrepo (
     $hccrepo_baseurl            = $hccrepo::params::hccrepo_baseurl,
     $hccrepo_enabled            = $hccrepo::params::hccrepo_enabled,
+    $hccrepo_priority           = $hccrepo::params::hccrepo_priority,
     $hccrepo_testing_baseurl    = $hccrepo::params::hccrepo_testing_baseurl,
     $hccrepo_testing_enabled    = $hccrepo::params::hccrepo_testing_enabled,
+    $hccrepo_testing_priority   = $hccrepo::params::hccrepo_testing_priority,
 ) inherits hccrepo::params {
 
     if $::osfamily == 'RedHat' and $::operatingsystem !~ /Fedora|Amazon/ {
@@ -17,12 +19,14 @@ class hccrepo (
             baseurl     => $hccrepo_baseurl,
             enabled     => $hccrepo_enabled,
             descr       => "HCC Packages for Enterprise Linux ${::lsbmajdistrelease} - \$basearch",
+            priority    => $hccrepo_priority,
         }
 
         yumrepo { 'hcc-testing':
             baseurl     => $hccrepo_testing_baseurl,
             enabled     => $hccrepo_testing_enabled,
             descr       => "HCC Packages for Enterprise Linux ${::lsbmajdistrelease} - Testing - \$basearch",
+            priority    => $hccrepo_testing_priority,
         }
 
     } else {
--- manifests/params.pp
+++ manifests/params.pp
@@ -2,7 +2,10 @@ class hccrepo::params {
 
     $hccrepo_baseurl = "http://t2.unl.edu/store/repos/hcc/${::lsbmajdistrelease}/hcc-el${::lsbmajdistrelease}/\$basearch"
     $hccrepo_enabled = 1
+    $hccrepo_priority = 'absent'
+
     $hccrepo_testing_baseurl = "http://t2.unl.edu/store/repos/hcc/${::lsbmajdistrelease}/hcc-el${::lsbmajdistrelease}-testing/\$basearch"
     $hccrepo_testing_enabled = 0
+    $hccrepo_testing_priority = 'absent'
 
 }
