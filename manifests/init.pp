be39c217a5f501c7e9b9001854e4baf46a0efb04
Garhan Attebury <garhan.attebury@unl.edu>
2014-08-04 10:42:16 -0500

    added GPG support, disabled by default -- maybe eventually yeah

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,9 +7,13 @@
 class hccrepo (
     $hccrepo_baseurl            = $hccrepo::params::hccrepo_baseurl,
     $hccrepo_enabled            = $hccrepo::params::hccrepo_enabled,
+    $hccrepo_gpgcheck           = $hccrepo::params::hccrepo_gpgcheck,
+    $hccrepo_gpgkey             = $hccrepo::params::hccrepo_gpgkey,
     $hccrepo_priority           = $hccrepo::params::hccrepo_priority,
     $hccrepo_testing_baseurl    = $hccrepo::params::hccrepo_testing_baseurl,
     $hccrepo_testing_enabled    = $hccrepo::params::hccrepo_testing_enabled,
+    $hccrepo_testing_gpgcheck   = $hccrepo::params::hccrepo_testing_gpgcheck,
+    $hccrepo_testing_gpgkey     = $hccrepo::params::hccrepo_testing_gpgkey,
     $hccrepo_testing_priority   = $hccrepo::params::hccrepo_testing_priority,
 ) inherits hccrepo::params {
 
@@ -19,6 +23,8 @@ class hccrepo (
             baseurl     => $hccrepo_baseurl,
             enabled     => $hccrepo_enabled,
             descr       => "HCC Packages for Enterprise Linux ${::lsbmajdistrelease} - \$basearch",
+            gpgcheck    => $hccrepo_gpgcheck,
+            gpgkey      => $hccrepo_gpgkey,
             priority    => $hccrepo_priority,
         }
 
@@ -26,6 +32,8 @@ class hccrepo (
             baseurl     => $hccrepo_testing_baseurl,
             enabled     => $hccrepo_testing_enabled,
             descr       => "HCC Packages for Enterprise Linux ${::lsbmajdistrelease} - Testing - \$basearch",
+            gpgcheck    => $hccrepo_testing_gpgcheck,
+            gpgkey      => $hccrepo_testing_gpgkey,
             priority    => $hccrepo_testing_priority,
         }
 
--- manifests/params.pp
+++ manifests/params.pp
@@ -2,10 +2,14 @@ class hccrepo::params {
 
     $hccrepo_baseurl = "http://t2.unl.edu/store/repos/hcc/${::lsbmajdistrelease}/hcc-el${::lsbmajdistrelease}/\$basearch"
     $hccrepo_enabled = 1
+    $hccrepo_gpgcheck = 0
+    $hccrepo_gpgkey = 'absent'
     $hccrepo_priority = 'absent'
 
     $hccrepo_testing_baseurl = "http://t2.unl.edu/store/repos/hcc/${::lsbmajdistrelease}/hcc-el${::lsbmajdistrelease}-testing/\$basearch"
     $hccrepo_testing_enabled = 0
+    $hccrepo_testing_gpgcheck = 0
+    $hccrepo_testing_gpgkey = 'absent'
     $hccrepo_testing_priority = 'absent'
 
 }
